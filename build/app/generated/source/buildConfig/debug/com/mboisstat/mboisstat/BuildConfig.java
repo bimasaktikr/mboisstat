732e22ddee6471045b3a9e11f63c055e17c0d0f8
bimasaktikr <bimasakti.kr@gmail.com>
2023-03-27 08:42:08 +0700

    Add HomeView

---
+++ build/app/generated/source/buildConfig/debug/com/mboisstat/mboisstat/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mboisstat.mboisstat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mboisstat.mboisstat";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -9,16 +9,52 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: RichText(
+              text: TextSpan(
+                  text: "Hai,  ",
+                  style: TextStyle(fontSize: 18),
+                  children: [
+                TextSpan(
+                    text: "Sahabat Data",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
+              ])),
+          centerTitle: false,
+          backgroundColor: Color(0xff238dcd),
+          elevation: 0,
         ),
-      ),
-    );
+        body: Stack(
+          children: [
+            ClipPath(
+              clipper: ClipPathClass(),
+              child: Container(
+                height: 250,
+                width: Get.width,
+                color: Color(0xff238dcd),
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class ClipPathClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height - 60);
+
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 60);
+
+    path.lineTo(size.width, 0.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    throw UnimplementedError();
   }
 }
